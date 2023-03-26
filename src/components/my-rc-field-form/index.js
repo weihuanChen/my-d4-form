94c266e66a0abc3ff0d35ee302a4bd8506e674dc
chenqi <13265255252@163.com>
2023-03-26 19:58:03 +0800

    init:基础架子

---
+++ src/components/my-rc-field-form/index.js
@@ -0,0 +1,12 @@
+import _Form from "./Form";
+import Field from "./Field";
+//自定义hook
+import useForm from "./useForm";
+
+const Form = _Form;
+
+Form.Field = Field;
+Form.useForm = useForm;
+
+export { Field, useForm };
+export default Form;
